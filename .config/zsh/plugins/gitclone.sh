7efaa69bb28f451acb92be260814ad402d1603f5
Mateusz Grzyb <grzybmateusz@protonmail.com>
2021-03-06 23:00:16 +0100

    gitclone

---
+++ .config/zsh/plugins/gitclone.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/zsh-users/zsh-autosuggestions.git
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+git clone https://github.com/b4b4r07/zsh-vimode-visual.git
